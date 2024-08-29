87fd6366d5ad9792293365637154006d87637c79
Niceforester <159060793+Niceforester@users.noreply.github.com>
2024-08-29 18:42:05 +0200

    Update Felder.java
    swappedd getter and setter position to make it einheitlich

--- SchiffeDaten/Felder.java
+++ SchiffeDaten/Felder.java
@@ -14,7 +14,7 @@ public abstract class Felder {
 
 	protected boolean treffer = false;
 	protected boolean versenkt = false;
-	
-	public abstract void setTreffer();
+
 	public abstract boolean getTreffer();
+	public abstract void setTreffer();
 }
